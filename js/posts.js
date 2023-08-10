eaedade062801916ca622ff9ddc21d2a346fd787
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2023-08-10 09:30:55 +0300

    adding new post (not published yet)
    Signed-off-by: Saif Kandil <74428638+k0T0z@users.noreply.github.com>

--- js/posts.js
+++ js/posts.js
@@ -13,4 +13,5 @@ const posts = [
     { id: 8, title: "GSoC '23 - Week 6 & 7: Ooooh great, Async is the big boss now...", postTitle: "GSoC &#39;23 - Week 6 &amp; 7: Ooooh great, <code>Async</code> is the big boss now...", fileName: "GSoC_2023_week_6_and_7_ooooh_great_async_is_the_big_boss_now", year: 2023 },
     { id: 9, title: "GSoC '23 - Week 8: Passing Midterm Evaluations", postTitle: "GSoC &#39;23 - Week 8: Passing Midterm Evaluations &#128525;", fileName: "GSoC_2023_week_8_midterm_evaluations", year: 2023 },
     { id: 10, title: "GSoC '23 - Week 9 & 10: Here we go again", postTitle: "GSoC &#39;23 - Week 9 &amp; 10: Here we go again", fileName: "GSoC_2023_week_9_and_10_here_we_go_again", year: 2023 }
+    // { id: 11, title: "The Ultimate GSoC Cracking Guide", postTitle: "The Ultimate GSoC Cracking Guide", fileName: "the_ultimate_gsoc_cracking_guide", year: 2023 }
 ];
