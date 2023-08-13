c0c2015d25b030406ad1340aa7d64333dbd903aa
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2023-08-13 09:43:28 +0300

    few improvements and bug fixes
    Signed-off-by: Saif Kandil <74428638+k0T0z@users.noreply.github.com>

--- js/footer.js
+++ js/footer.js
@@ -4,15 +4,11 @@ mainContent.insertAdjacentHTML("afterend",
     <data id="u-url-id" class="u-url"></data>
     <div class="wrapper">
         <h2 class="footer-heading">k0T0z</h2>
-        <div class="footer-col-wrapper">
-            <div class="footer-col footer-col-3">
-                <p>
-                    This personal website is built with HTML, CSS and deployed with GitHub pages. The code
-                    is licensed MIT license and can be found in the
-                    <a href="https://github.com/k0T0z/k0T0z.github.io/tree/master-published">GitHub repo</a>.
-                </p>
-            </div>
-        </div>
+        <p>
+            This personal website is built with HTML, CSS and deployed with GitHub pages. The code
+            is licensed MIT license and can be found in the
+            <a href="https://github.com/k0T0z/k0T0z.github.io/tree/master-published">GitHub repo</a>.
+        </p>
     </div>
 </footer>
     `
--- js/header.js
+++ js/header.js
@@ -3,20 +3,18 @@ mainContent.insertAdjacentHTML("beforebegin",
     `
 <header id="main-header" class="site-header" role="banner">
     <div class="wrapper">
-        <div id="site-header-h-card">
-            <div id="main-avatar-h-card">
-                <a id="main-header-avatar">
-                    <img id="main-avatar" alt="k0T0z avatar" />
-                </a>
-                <div id="site-header-v-card">
-                    <a id="home-page" class="site-title" rel="author">k0T0z</a>
-                    <span id="what-is-this">
-                        Just a place for me to dump my thoughts.
-                    </span>
-                </div>
-            </div>
-            <a id="about-page" class="site-title">About</a>
+        <a id="main-header-avatar" class="site-avatar">
+            <img id="main-avatar" alt="k0T0z avatar" />
+        </a>
+
+        <div class="site-info">
+          <h1 class="site-name"><a id="home-page">k0T0z</a></h1>
+          <p class="site-description">Just a place for me to dump my thoughts</p>
         </div>
+
+        <nav>
+          <a id="about-page" href="/about">About</a>
+        </nav>
     </div>
 </header>
     `
